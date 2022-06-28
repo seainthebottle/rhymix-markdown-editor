e662f65b394c62c475ae37471e8e90dd4a9dc3b0
seainthebottle <seainthebottle@gmail.com>
2022-06-28 17:49:03 +0900

    Developing

--- js/lib/textareacount.js
+++ js/lib/textareacount.js
@@ -7,19 +7,27 @@ const TextAreaCount = function () {
     this.lineCount = null;
     this.lineCounts = [];
     this.currentText = null;
-    this.fontHeight = 0;
-    this.textHeight = 0;
     this.editorWidth = 0;
+    this.standardNode = document.getElementById("StandardNode");
     
-    // 해당 TextArea의 정보를 추출한다.
+    // 해당 TextArea의 정보를 추출해 기억한다.
     this.getTextAreaInfo = function (elementSelector) {
+        this.standardNode.setAttribute("style", "position: absolute;visibility: hidden; \
+            height: auto; width: auto; white-space: nowrap;");
+
         var el = $(elementSelector);
-        // TODO: 해당 TextArea의 내부(padding 제외한) 폭, 너비, 폰트 종류 및 사이즈, text-height를 구한다.
+
+        this.editorWidth = el.width(); // padding 등을 뺀 실제 택스트가 들어가는 너비
+        this.standardNode.setAttribute("font-size") = el.css("font-size"); 
+        this.standardNode.setAttribute("font-family") = el.css("font-family"); 
 
     }
 
     // 계산을 수행할 텍스트를 등록한다.
     this.registerText (text) {
+        // 우선 각 줄의 너비를 구하고 이로써  overflow되는 줄을 파악해 총 줄수를 구하고 
+        // scrollHeight를 이용해 높이를 구해 줄수로 나누어 한줄당 높이도 구한다.
+        // 한줄당 높이를 구하는 것이 브라우저 차이로 인해 이 방법이 가장 안정적일 듯
         this.currentText = text;
         lines = text.split("\n");
         for(let textLine in lines)

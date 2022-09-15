5923906969d65a8e42f0af08835c509de834236f
seainthebottle <seainthebottle@gmail.com>
2022-09-15 22:02:39 +0900

    Reveted to JS for highlight tag not compatible

--- src/lib/additional-markdown-parser.js
+++ src/lib/additional-markdown-parser.js
@@ -1,15 +1,15 @@
 import {InlineContext, BlockContext, MarkdownConfig,
-    LeafBlockParser, LeafBlock, Line, Element} from "./markdown"
+    LeafBlockParser, LeafBlock, Line, Element, space} from "./markdown"
 import {tags as t} from "@lezer/highlight";
 
 /**
  * $, \(), \) 으로 Tex가 escape 된 부분을 파싱한다.
- * @param {string} node
+ * @param {string} node 
  * @param {string} mark 
  * @returns 
  */
-function parseTexInline(node: string, mark: string) {
-    return (cx: InlineContext, next: number, pos: number) => {
+function parseTexInline(node, mark) {
+    return (cx, next, pos) => {
         if (next != 36 || cx.char(pos + 1) == 36) { //'$'가 아니거나 '$$'일 때
             if (next != 92 || cx.char(pos + 1) != 40) return -1;} //'/('도 아니면 나간다.
         var q = (next == 36)? 0 : 1;
@@ -19,7 +19,7 @@ function parseTexInline(node: string, mark: string) {
             // 구분자와 같으면 끝에 다다렀으므로 추가
             if ((next == 36)    // '$'
                 || (next == 92 && cx.char(i + 1) == 41)) { // '\)'
-                    var p = (next == 36)? 0 : 1;
+                var p = (next == 36)? 0 : 1;
                 return cx.addElement(cx.elt(node, pos, i + 1 + p, elts.concat(cx.elt(mark, i, i + 1 + p))))
             }
         }
@@ -29,12 +29,12 @@ function parseTexInline(node: string, mark: string) {
 
 /**
  * $$, \[, \] 으로 Tex가 escape 된 부분을 파싱한다.
- * @param {string} node
+ * @param {string} node 
  * @param {string} mark 
  * @returns 
  */
-function parseTexBlock(node: string, mark: string) {
-    return (cx: InlineContext, next: number, pos: number) => {
+function parseTexBlock(node, mark) {
+    return (cx, next, pos) => {
         if (next != 36 || cx.char(pos + 1) != 36) { //'$$'가 아닐 때
             if (next != 92 || cx.char(pos + 1) != 91) return -1;} //'/['도 아니면 나간다.
         let elts = [cx.elt(mark, pos, pos + 2)]
@@ -43,8 +43,7 @@ function parseTexBlock(node: string, mark: string) {
             // 구분자와 같으면 끝에 다다렀으므로 추가
             if ((next == 36 && cx.char(i + 1) == 36)    // '$$'
                 || (next == 92 && cx.char(i + 1) == 93)) { // '\]'
-                    //console.log(cx.text.slice(pos - cx.offset))
-                    return cx.addElement(cx.elt(node, pos, i + 2, elts.concat(cx.elt(mark, i, i + 2))))
+                return cx.addElement(cx.elt(node, pos, i + 2, elts.concat(cx.elt(mark, i, i + 2))))
             }
       }
       return -1
@@ -53,12 +52,12 @@ function parseTexBlock(node: string, mark: string) {
 
 /**
  * ==로 마킹된 부분을 파싱한다. 
- * @param {string} node
+ * @param {string} node 
  * @param {string} mark 
  * @returns 
  */
-function parseMark(node: string, mark: string) {
-    return (cx: InlineContext, next: number, pos: number) => {
+function parseMark(node, mark) {
+    return (cx, next, pos) => {
         if (next != 61 || cx.char(pos + 1) != 61) return -1; //'=='가 아닐 때 나간다.
         let elts = [cx.elt(mark, pos, pos + 2)]
         for (let i = pos + 1; i < cx.end; i++) {
@@ -73,9 +72,9 @@ function parseMark(node: string, mark: string) {
 }
 
 /**
- * Tex inline을 파싱한다.
+ * 
  */
-export const mdpTexInline: MarkdownConfig = {
+export const mdpTexInline = {
     defineNodes: [
       {name: "TexInline", style: t.special(t.content)},
       {name: "TexInlineMark", style: t.processingInstruction}
@@ -88,9 +87,9 @@ export const mdpTexInline: MarkdownConfig = {
 }
 
 /**
- * Tex block inline을 파싱한다.
+ * 
  */
-export const mdpTexBlock: MarkdownConfig = {
+export const mdpTexBlock = {
     defineNodes: [
       {name: "TexBlock", style: t.special(t.content)},
       {name: "TexBlockMark", style: t.processingInstruction}
@@ -103,9 +102,9 @@ export const mdpTexBlock: MarkdownConfig = {
 }
 
 /**
- * Mark inline을 파싱한다.
+ * 
  */
-export const mdpMark: MarkdownConfig = {
+export const mdpMark = {
     defineNodes: [
       {name: "Mark", style: t.strong},
       {name: "MarkMark", style: t.processingInstruction}
@@ -119,25 +118,24 @@ export const mdpMark: MarkdownConfig = {
 /**
  *  mdpFootnote 보조용 클래스
  */
- class FootnoteParser implements LeafBlockParser {
+ class FootnoteParser {
     // 다음줄에 대하여 파서의 상태를 갱신한다.
     // 첫 줄에서는 호출되지 않는다.
     // true를 리턴하면 블록은 종료된다.
     // false를 리턴하면 다른 파서가 먼저 맞는 게 있으면 그것을 적용한다. 
-    nextLine() { 
-        console.log("nextLine")
+    nextLine(cx, line, leaf) { 
         return false;
     }
-
+    
     // 끝줄이면
-    finish(cx: BlockContext, leaf: LeafBlock) {
+    finish(cx, leaf) {
         return this.complete(cx, leaf);
     }
 
-    complete(cx: BlockContext, leaf: LeafBlock) {
+    complete(cx, leaf) {
         // 파싱 트리에 해당 잎들을 붙여준다.
         var reg = /^((?:(?:\[\^)|(?:\*\[)).+?\]:)/;
-        var count = (reg.test(leaf.content)) ? reg.exec(leaf.content)![1].length : 0;
+        var count = (reg.test(leaf.content)) ? reg.exec(leaf.content)[1].length : 0;
         cx.addLeafElement(leaf, cx.elt("Footnote", leaf.start, leaf.start + leaf.content.length,  [
             cx.elt("FootnoteMark", leaf.start, leaf.start + 2),
             cx.elt("FootnoteLabel", leaf.start + 2, leaf.start + count - 2),
@@ -151,7 +149,7 @@ export const mdpMark: MarkdownConfig = {
 /**
  * Reference와 Abbreviation block을 파싱한다.
  */
-export const mdpFootnote: MarkdownConfig = {
+export const mdpFootnote= {
     defineNodes: [
       {name: "Footnote", block: true},
       {name: "FootnoteLabel", style: t.keyword},
@@ -159,7 +157,7 @@ export const mdpFootnote: MarkdownConfig = {
     ],
     parseInline: [{
         name: "Footnote",
-        parse(cx, next, pos) {
+        parse(cx, _, pos) {
             const match = /^((?:(?:\[\^)|(?:\*\[)).+?\])/.exec(cx.text.slice(pos - cx.offset));
             if (match) {
                 const end = pos + match[0].length;
@@ -177,9 +175,9 @@ export const mdpFootnote: MarkdownConfig = {
     }],
     parseBlock: [{
         name: "Footnote",
-        leaf(cx, leaf) { console.log("test", leaf.content, (/^((?:(?:\[\^)|(?:\*\[)).+?\]:)/.test(leaf.content))); return (/^((?:(?:\[\^)|(?:\*\[)).+?\]:)/.test(leaf.content)) ? new FootnoteParser : null },
+        leaf(cx, leaf) { return (/^((?:(?:\[\^)|(?:\*\[)).+?\]:)/.test(leaf.content)) ? new FootnoteParser : null },
         // 아래 조건을 만족하면 한 줄을 안 띄워도 전 block을 마치고 바로 block을 시작한다.
-        endLeaf(cx, line, leaf) { return (/^((?:(?:\[\^)|(?:\*\[)).+?\]:)/.test(leaf.content)) ? true : false },
+        endLeaf(cl, line, leaf) { return (/^((?:(?:\[\^)|(?:\*\[)).+?\]:)/.test(leaf.content)) ? true : false },
         before: "LinkReference"
     }]
 }

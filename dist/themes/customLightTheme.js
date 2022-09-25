31e49cb6ab01218f9ba190fbac856814a9242040
seainthebottle <seainthebottle@gmail.com>
2022-09-25 22:34:36 +0900

    Add light custom theme

---
+++ dist/themes/customLightTheme.js
@@ -0,0 +1,19 @@
+var customLightTheme = {
+    style: {
+        foreground: "#101010",
+        background: "#ffffff",
+        highlightBackground: "#cccccc",
+        gutterBackground: "#f6f8fa"
+    },
+    highlight: {
+        keyword: {color: "#c678dd"}, 
+        strong: {fontWeight: "bold"}, 
+        emphasis: {fontStyle: "italic"}, 
+        strikethrough: {textDecoration: "line-through"}, 
+        link: {color: "#7d8799", textDecoration: "underline"}, 
+        heading: {fontWeight: "bold", color: "#e24242"},
+        processingInstruction: {color: "#60c050"},
+        tex: {color: "#61afef"}
+    },
+    isDark: false
+}
